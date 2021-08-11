3e8f0d1dc447cac27aa2a479fc506d1b0b4febad
Lautaro De Filippo <lautimax@hotmail.com>
2021-08-11 10:45:00 -0300

    Delete `commit-and-release.bat`

--- commit-and-release.bat
+++
@@ -1,2 +0,0 @@
-git add . && git commit -m "Fix ``commit-and-release.bat`` so that it stages new files" && git tag -a v1.3.2 -m "Fix ``commit-and-release.bat`` so that it stages new files" && git push && git push --tags
-pause
